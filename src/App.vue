2f3fcac764184f2d4631549db1609e6c3b67dfe8
CodersTokyo <info@coders.tolyo>
2020-11-13 10:54:31 +0700

    done

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,27 @@
         <button class="form-submit" @click="tinh()">Kết Quả</button>
       </div>
       <span> {{ deltaa }} </span><br />
-      <span> {{ mess }}</span
-      ><br />
-      <span>{{ empty }}</span
-      ><br />
-      <span> {{ double2 }}</span
-      ><br />
-      <span> {{ double }}</span>
+
+      <span v-if="err_msg">{{ err_msg }}</span>
+
+      <!-- <div v-if="danhtinh"> -->
+        <div v-if="delta == 0"> {{ x1 }}>
+
+        </div>
+
+        <div v-else-if="delta > 0">
+          {{ x1 }},{{ x2 }}
+          <span> </span>
+        </div>
+
+        <div v-else>
+          {{ x1 }}
+          <span> </span>
+          <button @click="reset()">retset</button>
+        </div>
+
+        
+      <!-- </div> -->
     </form>
   </div>
 </template>
@@ -41,37 +55,49 @@ export default {
 
   data() {
     return {
+      
       a: "",
       b: "",
       c: "",
       deltaa: "",
-      mess: "",
-      empty: "",
-      double: "",
-      double2: "",
+      x1: "",
+      x2: "",
+      err_msg: "",
     };
   },
   components: {},
 
   methods: {
     tinh() {
-      const delta = this.b * this.b - 4 * this.a * this.c;
-       this.deltaa = "  delta la " +delta ;
-     
-      var x1, x2;
-      if (delta === 0) {
-        x1 = -this.b / (2 * this.a);
-        this.mess = 'nghiem khep x1 = x2 = ' +x1;
-      } else if (delta < 0) {
-        this.empty = "phuong trinh vo nghiem";
-      } else {
-        x1 = (-this.b - Math.sqrt(delta)) / (2 * this.a);
-        this.double2 = "pt co 2 nghiem phan biet  x1 la :" + x1;
-        x2 = (-this.b + Math.sqrt(delta)) / (2 * this.a);
-        this.double = "x2 la:" + x2;
+      
+      
+      if (!Number.isInteger(parseInt(this.a)) || !Number.isInteger(parseInt(this.b)) ||!Number.isInteger(parseInt(this.a))) {
+        this.err_msg = " loi k phai so" ;
+        return
+        
       }
-      console.log(1)
 
+      const delta = this.b * this.b - 4 * this.a * this.c;
+      this.deltaa = "delta la " + delta;
+       if (delta === 0) {
+        this.x1 = "x1 :" + -this.b / (2 * this.a);
+      } else if (delta < 0) {
+        this.x1 = "vo nghiem";
+      } else {
+        this.x1 = "x1:" + (-this.b - Math.sqrt(delta)) / (2 * this.a);
+        this.x2 = "x2 :" + (-this.b + Math.sqrt(delta)) / (2 * this.a);
+      }
+    },
+
+    reset() {
+      this.danhtinh = false
+      this.a =''
+      this.b=''
+      this.c=''
+      this.deltaa=''
+      this.err_msg =""
+      this.x1=''
+      this.x2=''
     },
   },
 };
